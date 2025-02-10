b26fd3af685c275f8b9292d9f6120dced8a35172
SALEH <salehenab850@gmail.com>
2025-02-10 17:31:14 +0200

    refactor: use new type and env schema validation

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,20 @@
 import { z } from "zod";
 import "dotenv/config";
 
+type StringValue = `${number}${'s' | 'm' | 'h' | 'd' | 'w' | 'y'}`;
+
+
 const envSchema = z.object({
     PORT: z.coerce.number().default(3000),
     DATABASE_URL: z.string().url(),
+    ACCESS_TOKEN_TTL: z.custom<StringValue>((val) => {
+        return /^\d+[smhdwy]$/.test(val as string);
+    }),
+    REFRESH_TOKEN_TTL: z.custom<StringValue>((val) => {
+        return /^\d+[smhdwy]$/.test(val as string);
+    }),
+    ACCESS_TOKEN_PUBLIC_KEY: z.string().startsWith("-----BEGIN PUBLIC KEY-----"),
+    ACCESS_TOKEN_PRIVATE_KEY: z.string().startsWith("-----BEGIN RSA PRIVATE KEY-----")
 });
 
 const env = envSchema.parse(process.env);
