841a9dfae90f7c01c34a0b16b7b6da902cf029de
SALEH <salehenab850@gmail.com>
2025-02-10 17:25:08 +0200

    refactor: change file name from hashPassword to password for more clarity

--- src/middlewares/hashPassword.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt';
-
-export const hashPassword = async (plainPassword: string): Promise<string> => {
-    const hashedPassword = await bcrypt.hash(plainPassword, 12)
-    return hashedPassword;
-}
