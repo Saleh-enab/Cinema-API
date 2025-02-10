bd36127acf2128df9e6495d71b668e70bb33fa59
SALEH <salehenab850@gmail.com>
2025-02-10 13:10:47 +0200

    feat: create a new router and use json parsing

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express, { Request, Response } from "express";
 import env from "./env";
 import { errorHandler } from "./middlewares/errorHandler";
+import { customerRouter } from "./routes/customer.routes";
 
 const app = express();
-
 const port = env.PORT;
+
+app.use(express.json());
 
 app.use("/healthcheck", (req: Request, res: Response) => {
     res.send("الصحه حديد");
 });
+
+app.use('/customer', customerRouter)
 
 
 app.use(errorHandler)
