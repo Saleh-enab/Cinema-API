60e4265f8517b4fe7f73b661a7a51d4393e267e8
SALEH <salehenab850@gmail.com>
2025-02-10 17:27:19 +0200

    chore: add new property to Error class for refering errors types

--- src/types/customError.ts
+++ src/types/customError.ts
@@ -4,11 +4,13 @@ interface BasicError {
 }
 
 export class CustomError extends Error implements BasicError {
+    type: string
     status: number
     message: string
-    constructor(status: number, message: string) {
+    constructor(status: number, message: string, type: string) {
         super();
         this.message = message;
         this.status = status;
+        this.type = type
     }
 }
