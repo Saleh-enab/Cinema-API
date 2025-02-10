51210fa0eb25cabb537002e2ebb4fc3805905413
SALEH <salehenab850@gmail.com>
2025-02-10 13:12:36 +0200

    chore: change DOB validtion to regex instead of Date & fix the typo

--- src/schemas/customer.schema.ts
+++ src/schemas/customer.schema.ts
@@ -1,13 +1,17 @@
 import { z } from 'zod';
 
-export const customrSchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    dateOfBirth: z.date(),
-    phone: z.string(),
-    password: z.string(),
-    confirmPassword: z.string()
-}).refine((data) => data.password === data.confirmPassword, {
+export const customerSchema = z.object({
+    body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+        dateOfBirth: z.string()
+            .regex(/^\d{2}\/\d{2}\/\d{4}$/, "Invalid date format. Use DD/MM/YYYY."),
+        phone: z.string(),
+        password: z.string(),
+        confirmPassword: z.string()
+    })
+
+}).refine((data) => data.body.password === data.body.confirmPassword, {
     message: "Passwords don't match",
     path: ["confirm"]
 })
