bf4493a0dabc10646bcd74eca77517a9536aed26
SALEH <salehenab850@gmail.com>
2025-02-12 10:46:21 +0200

    intall & use cookie-parser package

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express, { Request, Response } from "express";
 import env from "./env";
 import { errorHandler } from "./middlewares/errorHandler";
 import { customerRouter } from "./routes/customer.routes";
+import cookireParser from 'cookie-parser';
 
 const app = express();
 const port = env.PORT;
 
 app.use(express.json());
+app.use(cookireParser())
 
 app.use("/healthcheck", (req: Request, res: Response) => {
     res.send("الصحه حديد");
